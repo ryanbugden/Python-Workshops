b6ae92d83aacefc7122b0d09516598a6e0812c25
Ryan Bugden <rbugden@gmail.com>
2024-03-24 20:39:23 -0400

    Finish in-person DrawBot comments
    Next is RoboFont

--- 2024_In_Person/02 - DrawBot/transformations.py
+++ 2024_In_Person/02 - DrawBot/transformations.py
@@ -1,16 +1,22 @@
-
-for page in range(20):
+# Loop through 20 pages
+for page_number in range(20):
+    # Make a page
     newPage()
+    # Get the center point of the page
     center_point = (width()/2, height()/2)
-    rotate(3*page, center=center_point)
-    scale(page/2, center=center_point)
+    # Rotate, more for each page
+    rotate(3 * page_number, center=center_point)
+    scale(page_number / 2, center=center_point)
     skew(-5, center=center_point)
-    # Make the line
+    # Make the art
+    # Draw the line
     stroke(0)
     strokeWidth(50)
     line((10, 10), (990, 990))
     line((990, 10), (10, 990))
+    # Draw a rect, using a radius for some reason
     my_radius = 90
     rect(width()/2 - my_radius, height()/2 - my_radius, my_radius*2, my_radius*2)
 
-saveImage("warped_guy.gif")
+# Save all pages into 1 GIF
+saveImage("_output/warped_guy.gif")
--- 2024_In_Person/02 - DrawBot/ufo_bezier_path.py
+++ 2024_In_Person/02 - DrawBot/ufo_bezier_path.py
@@ -1,14 +1,21 @@
 from fontParts.world import RFont
 
-my_font = RFont('/Users/Ryan/Documents/Git/public/Python Workshops/2024_In_Person/Test UFOs/Source_Serif_Caption-Bold.ufo')
+# Get a font object
+my_font = RFont('../../_test_UFOs/Source_Serif_Caption-Bold.ufo')
 
+# Get a glyph object from font
+my_a_glyph = my_font["a"]
+# Tell me how many contours this glyph has
+print(len(my_a_glyph.contours))
+
+# Make an empty Bezier path
 path = BezierPath()
 print(path.onCurvePoints)
 
-my_a_glyph = my_font["a"]
-print(len(my_a_glyph.contours))
-
+# Draw the glyph object into the Bezier path
 my_a_glyph.draw(path)
+# Tell me the off-curves, for kicks.
 print(path.offCurvePoints)
 
+# Finally, draw it!
 drawPath(path)
