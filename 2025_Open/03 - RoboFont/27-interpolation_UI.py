0b07e0e0b20346b1ef77dcbf4f1e74ce700b0543
Ryan Bugden <rbugden@gmail.com>
2025-04-28 12:26:18 -0400

    Update 27-interpolation_UI.py

--- 2025_Open/03 - RoboFont/27-interpolation_UI.py
+++ 2025_Open/03 - RoboFont/27-interpolation_UI.py
@@ -9,10 +9,10 @@ class Demo(ezui.WindowController):
         content = """
         * TwoColumnForm         @form
 
-        > : Source Font:
-        > (Font 1 ...)          @sourceFont
-        > : Target Font:
-        > (Font 2 ...)          @targetFont
+        > : Font 0:
+        > (Font 0 ...)          @font0
+        > : Font 1:
+        > (Font 1 ...)          @font1
         
         > : Factor:
         > [_0_]                 @factor
@@ -24,8 +24,8 @@ class Demo(ezui.WindowController):
         # Set some styling for the things you put in content 
         descriptionData=dict(
             form=dict(
-                titleColumnWidth=80,
-                itemColumnWidth=200,
+                titleColumnWidth=60,
+                itemColumnWidth=220,
             ),
             factor=dict(
                 valueWidth=60,
@@ -44,14 +44,14 @@ class Demo(ezui.WindowController):
             controller=self
         )
         # Loop through each of the dropdown identifiers
-        for identifier in ["sourceFont", "targetFont"]:
+        for identifier in ["font0", "font1"]:
             # Get the UI element
             drop_down = self.w.getItem(identifier)
             # Add some strings representing the fonts to the list
             items = [f.info.familyName + " " + f.info.styleName for f in self.fonts]
             drop_down.setItems(items)
             # For the second dropdown, change the selection to the second font (if there is more than one font open)
-            if identifier == "targetFont" and len(self.fonts) > 1:
+            if identifier == "font1" and len(self.fonts) > 1:
                 drop_down.set(1)
 
     # The started function runs second
@@ -65,9 +65,9 @@ class Demo(ezui.WindowController):
         # Create a new_font
         new_font = RFont(showInterface=False)
         # Get the font from the first dropdown
-        min_font = self.fonts[self.w.getItem("sourceFont").get()]
+        min_font = self.fonts[self.w.getItem("font0").get()]
         # Get the font from the second dropdown
-        max_font = self.fonts[self.w.getItem("targetFont").get()]
+        max_font = self.fonts[self.w.getItem("font1").get()]
         # (Optional) Check if they are interpolable (FYI: this function is not perfect, so don't rely on it usually.)
         compatible = min_font.isCompatible(max_font)
         if not compatible:
